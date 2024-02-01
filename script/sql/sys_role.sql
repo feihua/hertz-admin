702afd48ac548e58639397342fe8e8183ac1fff7
koobe <1002219331@qq.com>
2024-02-01 11:41:06 +0800

    更新sql脚本

---
+++ script/sql/sys_role.sql
@@ -0,0 +1,3 @@
+INSERT INTO rustdb.sys_role (id, role_name, status_id, sort, remark, create_time, update_time) VALUES (1, '超级管理员', 1, 1, '全部权限', '2018-02-06 15:47:52', '2018-03-30 15:12:07');
+INSERT INTO rustdb.sys_role (id, role_name, status_id, sort, remark, create_time, update_time) VALUES (3, '演示角色', 1, 1, '仅有查看功能', '2018-12-28 18:23:38', '2018-12-28 18:23:38');
+INSERT INTO rustdb.sys_role (id, role_name, status_id, sort, remark, create_time, update_time) VALUES (4, '121', 0, 1, '121211', '2022-07-14 16:05:07', '2022-07-15 16:16:17');
---
+++ script/sql/sys_user_role.sql
@@ -0,0 +1,5 @@
+INSERT INTO rustdb.sys_user_role (id, user_id, role_id, status_id, sort, create_time, update_time) VALUES (1, 2, 3, 1, 1, '2022-07-15 14:13:46', '2022-07-15 14:13:46');
+INSERT INTO rustdb.sys_user_role (id, user_id, role_id, status_id, sort, create_time, update_time) VALUES (2, 12, 3, 1, 1, '2022-07-30 16:51:55', '2022-07-30 16:51:55');
+INSERT INTO rustdb.sys_user_role (id, user_id, role_id, status_id, sort, create_time, update_time) VALUES (3, 13, 4, 1, 1, '2022-07-30 17:03:55', '2022-07-30 17:03:55');
+INSERT INTO rustdb.sys_user_role (id, user_id, role_id, status_id, sort, create_time, update_time) VALUES (4, 3, 3, 1, 1, '2022-07-30 17:03:55', '2022-07-30 17:03:55');
+INSERT INTO rustdb.sys_user_role (id, user_id, role_id, status_id, sort, create_time, update_time) VALUES (5, 1, 1, 1, 1, '2022-07-30 17:03:55', '2022-07-30 17:03:55');
