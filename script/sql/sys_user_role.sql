ee7d3df783f5e193f697f5bde0d4d77383a1ef1f
koobe <1002219331@qq.com>
2024-02-22 11:34:54 +0800

    sql整理

--- script/sql/sys_user_role.sql
+++ script/sql/sys_user_role.sql
@@ -1,5 +1,11 @@
-INSERT INTO rustdb.sys_user_role (id, user_id, role_id, status_id, sort, create_time, update_time) VALUES (1, 2, 3, 1, 1, '2022-07-15 14:13:46', '2022-07-15 14:13:46');
-INSERT INTO rustdb.sys_user_role (id, user_id, role_id, status_id, sort, create_time, update_time) VALUES (2, 12, 3, 1, 1, '2022-07-30 16:51:55', '2022-07-30 16:51:55');
-INSERT INTO rustdb.sys_user_role (id, user_id, role_id, status_id, sort, create_time, update_time) VALUES (3, 13, 4, 1, 1, '2022-07-30 17:03:55', '2022-07-30 17:03:55');
-INSERT INTO rustdb.sys_user_role (id, user_id, role_id, status_id, sort, create_time, update_time) VALUES (4, 3, 3, 1, 1, '2022-07-30 17:03:55', '2022-07-30 17:03:55');
-INSERT INTO rustdb.sys_user_role (id, user_id, role_id, status_id, sort, create_time, update_time) VALUES (5, 1, 1, 1, 1, '2022-07-30 17:03:55', '2022-07-30 17:03:55');
+create table sys_user_role
+(
+    id          bigint auto_increment comment '主键'
+        primary key,
+    user_id     bigint   default 0                 not null comment '用户ID',
+    role_id     bigint                             not null comment '角色ID',
+    create_time datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time datetime                           null on update CURRENT_TIMESTAMP comment '修改时间'
+)
+    comment '角色用户关联表';
+
