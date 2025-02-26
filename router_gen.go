103c7ba01511584bb88e190b6ab22be6089d73b0
liufeihua <1002219331@qq.com>
2025-02-26 17:55:07 +0800

    refactor(router): update router package import path
    - Change the router package import path from
      "github.com/feihua/hertz-admin/biz/router" to
      "github.com/feihua/hertz-admin/biz/router/system"

--- router_gen.go
+++ router_gen.go
@@ -4,7 +4,7 @@ package main
 
 import (
 	"github.com/cloudwego/hertz/pkg/app/server"
-	router "github.com/feihua/hertz-admin/biz/router"
+	router "github.com/feihua/hertz-admin/biz/router/system"
 )
 
 // register registers all routers.
