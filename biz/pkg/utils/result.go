7863aa105a3375d0f9f8df45536abc652907592b
liufeihua <1002219331@qq.com>
2025-02-28 10:53:47 +0800

    refactor(biz): 重构 HTTP 工具和结果处理
    - 移除 http_util.go 中的硬编码 Authorization 头
    -重构 result.go 中的 BaseResponse 结构体方法：
      - Error 方法现在接受 RequestContext 参数
      - Success 方法改为直接调用 JSON 响应
      - SuccessPage 方法改为直接调用 JSON 响应- 在 result.go 中添加必要的包导入

--- biz/pkg/utils/result.go
+++ biz/pkg/utils/result.go
@@ -1,5 +1,10 @@
 package utils
 
+import (
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+)
+
 type BaseResponse struct {
 	Code  int32       `json:"code"`
 	Msg   string      `json:"msg"`
@@ -7,37 +12,27 @@ type BaseResponse struct {
 	Total int64       `json:"total"`
 }
 
-func (r BaseResponse) Error(msg error) *BaseResponse {
-
-	return &BaseResponse{
-		Code: 1,
-		Msg:  msg.Error(),
-	}
-}
-
-func (r BaseResponse) ErrorMsg(msg string) *BaseResponse {
-
-	return &BaseResponse{
+func (r BaseResponse) Error(c *app.RequestContext, msg string) {
+	c.JSON(consts.StatusOK, &BaseResponse{
 		Code: 1,
 		Msg:  msg,
-	}
+	})
 }
 
-func (r BaseResponse) Success(data interface{}) BaseResponse {
-
-	return BaseResponse{
+func (r BaseResponse) Success(c *app.RequestContext, data interface{}) {
+	c.JSON(consts.StatusOK, &BaseResponse{
 		Code: 0,
 		Msg:  "操作成功",
 		Data: data,
-	}
+	})
+
 }
 
-func (r BaseResponse) SuccessPage(data interface{}, total int64) BaseResponse {
-
-	return BaseResponse{
+func (r BaseResponse) SuccessPage(c *app.RequestContext, data interface{}, total int64) {
+	c.JSON(consts.StatusOK, &BaseResponse{
 		Code:  0,
 		Msg:   "操作成功",
 		Data:  data,
 		Total: total,
-	}
+	})
 }
